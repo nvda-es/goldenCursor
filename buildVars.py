23e51d9a0dc74c920fe600d61f8c8631a987be16
José Manuel Delicado Alcolea <jmdaweb@hotmail.com>
2022-02-22 13:09:06 +0100

    Golden cursor 6.1

--- buildVars.py
+++ buildVars.py
@@ -25,11 +25,11 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description": _("Allows you to control the mouse movement with the keyboard. for further details please visit the addon guide help."),
 	# version
-	"addon_version": "5.1",
+	"addon_version": "6.1",
 	# Author(s)
 	"addon_author": "Salah Atair <atair1978@gmail.com>, Wafeeq Taher, Joseph Lee <joseph.lee22590@gmail.com>, Abdel <abdelkrim.bensaid@gmail.com>",
 	# URL for the add-on documentation support
-	"addon_url": "https://addons.nvda-project.org/",
+	"addon_url": "https://addons.nvda-project.org/addons/goldenCursor.en.html",
 	# Documentation file name
 	"addon_docFileName": "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
